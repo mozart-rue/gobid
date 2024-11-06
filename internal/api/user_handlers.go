6bbc6b4bd47f6594e6b3973a4fc2c13b32367266
Mozart <mozart@toteduca.com>
2024-11-06 08:39:10 -0300

    feat: start auth users handlers

---
+++ internal/api/user_handlers.go
@@ -0,0 +1,9 @@
+package api
+
+import "net/http"
+
+func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {}
+
+func (api *Api) handleSignInUser(w http.ResponseWriter, r *http.Request) {}
+
+func (api *Api) handleLogOutUser(w http.ResponseWriter, r *http.Request) {}
