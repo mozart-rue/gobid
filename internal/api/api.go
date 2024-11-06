e4f3f2a2819db55189d3c3f85b8470fbc5cf73e3
Mozart <mozart@toteduca.com>
2024-11-06 17:38:21 -0300

    feat: set api and user handlers

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
+	"github.com/mozart-rue/gobid/internal/service"
 )
 
 type Api struct {
-	Router *chi.Mux
+	Router      *chi.Mux
+	UserService service.UserService
+	Session     *scs.SessionManager
 }
--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -1,9 +1,50 @@
 package api
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
 
-func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {}
+	"github.com/mozart-rue/gobid/internal/jsonutils"
+	"github.com/mozart-rue/gobid/internal/service"
+	"github.com/mozart-rue/gobid/internal/usecase/user"
+)
 
-func (api *Api) handleSignInUser(w http.ResponseWriter, r *http.Request) {}
+func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {
+	data, problems, err := jsonutils.DecodevalidJson[user.CreateUserRequest](r)
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	id, err := api.UserService.CreateUser(
+		r.Context(),
+		data.UserName,
+		data.Email,
+		data.Password,
+		data.Bio,
+	)
+
+	if err != nil {
+		if errors.Is(err, service.ErrDuplicatedEmailOrUsername) {
+			_ = jsonutils.EncodeJson(w, r, http.StatusConflict, map[string]any{
+				"message": "username or email already exists",
+			})
+			return
+		}
+	}
+
+	_ = jsonutils.EncodeJson(w, r, http.StatusConflict, map[string]any{
+		"userId": id,
+	})
+}
+
+func (api *Api) handleSignInUser(w http.ResponseWriter, r *http.Request) {
+	_, problems, err := jsonutils.DecodevalidJson[user.SignInUserRequest](r)
+	if err != nil {
+		jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+}
 
 func (api *Api) handleLogOutUser(w http.ResponseWriter, r *http.Request) {}
