2c2f793cf47e6c6042d29be4809020f89c1e9830
Mozart <mozart@toteduca.com>
2024-11-07 18:03:52 -0300

    feat: handle sign in and logout

--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -39,12 +39,52 @@ func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (api *Api) handleSignInUser(w http.ResponseWriter, r *http.Request) {
-	_, problems, err := jsonutils.DecodevalidJson[user.SignInUserRequest](r)
+	data, problems, err := jsonutils.DecodevalidJson[user.SignInUserRequest](r)
 	if err != nil {
 		jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
 		return
 	}
 
+	id, err := api.UserService.SignInUser(r.Context(), data.Email, data.Password)
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidCredentials) {
+			jsonutils.EncodeJson(w, r, http.StatusUnauthorized, map[string]any{
+				"message": "invalid credentials",
+			})
+			return
+		}
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"message": "Something went wrong while authenticating the user",
+		})
+		return
+	}
+
+	err = api.Session.RenewToken(r.Context())
+	if err != nil {
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"message": "Something went wrong while authenticating the user",
+		})
+		return
+	}
+
+	api.Session.Put(r.Context(), "AuthenticatedUserID", id)
+
+	jsonutils.EncodeJson(w, r, http.StatusOK, map[string]any{
+		"message": "User authenticated",
+	})
 }
 
-func (api *Api) handleLogOutUser(w http.ResponseWriter, r *http.Request) {}
+func (api *Api) handleLogOutUser(w http.ResponseWriter, r *http.Request) {
+	err := api.Session.RenewToken(r.Context())
+	if err != nil {
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"message": "Something went wrong while logging out the user",
+		})
+		return
+	}
+
+	api.Session.Remove(r.Context(), "AuthenticatedUserID")
+	jsonutils.EncodeJson(w, r, http.StatusOK, map[string]any{
+		"message": "User logged out",
+	})
+}
